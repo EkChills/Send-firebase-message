d39d9380b894db88e23c7fbf6c86259d0cfcd05a
Ekene <ekeneezeifeomasam27@gmail.com>
2023-09-08 21:09:03 +0100

    shawty

---
+++ src/app/api/sendtopic/route.ts
@@ -0,0 +1,41 @@
+import {applicationDefault, initializeApp, refreshToken} from 'firebase-admin/app'
+import { Message, getMessaging } from 'firebase-admin/messaging';
+import { NextRequest, NextResponse } from 'next/server';
+
+type MessageType = {
+  connectionCode:string;
+  courseCode:string;
+  topic:string;
+}
+
+export async function POST(req:NextRequest) {
+  try {
+    const {connectionCode, courseCode, topic}:MessageType = await req.json()
+
+    if(!connectionCode || !courseCode) {
+      return new NextResponse('bad request', {status:400})
+    }
+    
+    const message:Message = {
+      notification: {
+        title: connectionCode,
+        body: courseCode,
+      },
+      topic:topic,
+    };
+  
+    // const app = initializeApp({projectId:'attendance-mgmt-kwasu', credential:refreshToken('AIzaSyCza-5FM9SQlM70vPDBd-cNSil6H6EaGvE')})
+    initializeApp({
+      projectId:'attendance-mgmt-kwasu',
+      credential: applicationDefault(),
+      // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+  });
+  const res = await getMessaging().send(message)
+  console.log('Successfully sent message:', res);
+  return NextResponse.json({msg:'success'})
+    
+  } catch (error) {
+    console.log('Error sending message:', error);
+  }
+
+}
