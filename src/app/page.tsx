f8a49b2732182d5d67fccbb92e825ab858d081dd
Ekene <ekeneezeifeomasam27@gmail.com>
2023-09-09 14:35:19 +0100

    ini

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+"use client"
+
 import React from 'react'
+import {initializeApp} from 'firebase-admin/app'
+import admin from 'firebase-admin'
+import servAct from  '../../service-account.json'
+import {v4 as uuid} from 'uuid'
+
+
 
 const body =  {
   connectionCode:"wanna lig?",
@@ -6,13 +14,24 @@ const body =  {
   topic:"100"
 }
 
-export default async function page() {
+
+
+export default  function Home() {
   // const res = await fetch('https://send-firebase-message.vercel.app/api/sendtopic', {
   //   method:'POST',
   //   body:body as any
   // })
   // const data = await res.json()
   // console.log(data);
+
+  React.useEffect(() => {
+    const app = initializeApp({
+      projectId:'attendance-mgmt-kwasu',
+      credential: admin.credential.cert(servAct as any),
+    }, uuid())
+    console.log(app);
+    
+  },[])
   
   return (
     <div>page</div>
