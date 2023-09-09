77b8bdcc222a42f6eb35ebeeb5362df196802287
Ekene <ekeneezeifeomasam27@gmail.com>
2023-09-09 14:49:06 +0100

    make

--- src/app/api/sendtopic/route.ts
+++ src/app/api/sendtopic/route.ts
@@ -14,6 +14,7 @@ type MessageType = {
 }
 
 export async function POST(req:NextRequest) {
+  console.log(  process.env.GOOGLE_APPLICATION_CREDENTIALS);
   process.env.GOOGLE_APPLICATION_CREDENTIALS 
   const {connectionCode, courseCode, topic, appName}:MessageType = await req.json()
   const servAct = require('../../../../service-account.json')
